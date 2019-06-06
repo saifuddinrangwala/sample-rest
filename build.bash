71b30aa940964fd148f13ebbc1980606d730104a
Ubuntu <ubuntu@ip-10-0-2-242.ec2.internal>
2019-06-06 16:38:40 +0000

    Update build script to push docker image to hub

--- build.bash
+++ build.bash
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-sudo docker build --network=host -t sample-rest:$(git rev-parse --short HEAD) -t sample-rest:latest .
+sudo docker build --network=host -t saifuddin53/sample-rest:$(git rev-parse --short HEAD) -t saifuddin53/sample-rest:latest .
+
+sudo docker login
+
+sudo docker push saifuddin53/sample-rest
+
 
 
 
